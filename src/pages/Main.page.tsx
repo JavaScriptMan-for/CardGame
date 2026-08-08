c7a1893d1517a46c774d1a8ac11c7949ce51d2ba
JavaScriptMan <minevskiia10@gmail.com>
2026-08-08 20:35:39 +0300

    main page

--- src/pages/Main.page.tsx
+++ src/pages/Main.page.tsx
@@ -9,7 +9,7 @@ const MainPage: FC = () => {
     return (
         <div id="main_page">
             <h1>Карточная игра Дурак</h1>
-            <h2>Добро пожаловать в игру!</h2>
+            <h2>Добро пожаловать!</h2>
             <button onClick={() => nav(Links.GAME)} type="button">Начать игру</button>
         </div>
     )
